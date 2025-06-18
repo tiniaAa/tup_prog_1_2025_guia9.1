51b30ac56abc6bd26b55285e9de2660d96e1604f
tiniaAa <franciscovolovik@gmail.com>
2025-06-17 21:25:33 -0300

    .

---
+++ Guia 9.1/EJ 2.2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EJ_2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region definir var
+            int[] numeros;
+
+            #endregion
+
+            #region inicializar var 
+             numeros = new int[100];
+
+
+            #endregion
+
+            #region llenar vector 
+
+
+
+            #endregion
+
+
+        }
+    }
+}
